c5db454cc6a28d1ecd4f87e09217fc34b5f148d9
SubhadityaMukherjee <msubhaditya@gmail.com>
2022-01-24 11:18:51 +0100

    -n

--- pusher.sh
+++ pusher.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 black "." && isort .
 # create scripts if there are any notebook files
-for i in $(fd --glob "*.py"); do jupytext --to notebook $i; done
+for i in $(fdfind --glob "*.py"); do jupytext --to notebook $i; done
 if [ ! -z $1 ]; then
         git add . && git commit -m $1 && git push
 fi
